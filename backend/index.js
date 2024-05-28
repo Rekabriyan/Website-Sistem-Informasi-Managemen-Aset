b47014073cbbf612196b4c00f432829809559342
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-28 12:27:49 +0700

    add dependency and all route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import userRouter from "./routes/userRoute.js";
 import assetRouter from "./routes/assetRoute.js";
 import reqassetRouter from "./routes/requestassetRoute.js";
+import historyRouter from "./routes/historyRoute.js";
+import peminjamanRouter from "./routes/peminjamanRoute.js";
+import mutasiRouter from "./routes/mutasiRoute.js";
+import laporanRouter from "./routes/laporanRoute.js";
 
 import cors from "cors";
 
@@ -15,6 +19,10 @@ app.use(cors());
 app.use(userRouter);
 app.use(assetRouter);
 app.use(reqassetRouter);
+app.use(historyRouter);
+app.use(peminjamanRouter);
+app.use(mutasiRouter);
+app.use(laporanRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
