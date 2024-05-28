40a532f652f4e9eb05ff021700634e71b562aac1
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-28 10:41:10 +0700

    update dependency

--- backend/config/Database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('asset_management', 'postgres', '123', {
-    host: 'localhost',
-    dialect: 'postgres', // Menggunakan PostgreSQLA
-    port: 5432, // Port default PostgreSQL
-})
-
-export default db;
