2bd650b799861e62bd87fa2d72d33565d55d3543
Rekabriyan <rekabriyanch08@gmail.com>
2024-06-17 19:51:30 +0700

    fix dashboard

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../components/DashboardGA/Sidebar";
 import Navbar from "../components/DashboardGA/Navbar";
-import Table from "../components/ExamplesTable";
 import axios from "axios";
 import Swal from "sweetalert2";
 import Cookies from "js-cookie";
-import { Card, Row, Col } from 'react-bootstrap';
-import { Pie, Bar } from 'react-chartjs-2';
+import { Card, Row, Col, Container } from 'react-bootstrap';
 
 const Report = () => {
   const [allUser, setAllUser] = useState([]);
@@ -23,44 +21,15 @@ const Report = () => {
     loadUser();
   }, []);
 
-
   const loadUser = async () => {
     try {
-      const result = await axios.get(
-        `http://localhost:5005/users`);
+      const result = await axios.get(`http://localhost:5005/users`);
       setAllUser(result.data.data);
       console.log(result.data.data);
     } catch (error) {
       console.error("Error loading user data:", error);
     }
   };
-
-  // const deleteIncome = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const data = await axios.delete(
-  //       `http://localhost:5000/income/${deletesIncome.id_income}`,
-  //       { validateStatus: false }
-  //     );
-  //     if (data.status === 200) {
-  //       Swal.fire({
-  //         icon: "success",
-  //         title: "Income Deleted!",
-  //         text: data.data.msg,
-  //       });
-  //     } else {
-  //       Swal.fire({
-  //         icon: "error",
-  //         title: "Income Failed!",
-  //         text: data.data.msg,
-  //       });
-  //     }
-  //     $("#deleteincomemodal").modal("hide");
-  //     loadUser();
-  //   } catch (error) {
-  //     console.error("Error deleting income:", error);
-  //   }
-  // };
 
   const addUser = async (e) => {
     e.preventDefault();
@@ -82,13 +51,9 @@ const Report = () => {
           text: data.data.msg,
         });
       }
-      // $("#addincomemodal").modal("hide");
 
-      // Reload the income data after adding
       loadUser();
-
-      // Clear the input fields
-      setNewUser({ username: "", password: ""});
+      setNewUser({ username: "", password: "" });
     } catch (error) {
       console.error("Error adding user:", error);
     }
@@ -109,58 +74,60 @@ const Report = () => {
       <div id="content-wrapper" className="d-flex flex-column">
         <div id="content">
           <Navbar />
-          <Row className="mb-4">
-            <Col>
-              <Card className="text-center">
-                <Card.Body>
-                  <h4>1</h4>
-                  <p>Permintaan Aset</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="text-center">
-                <Card.Body>
-                  <h4>1</h4>
-                  <p>Peminjaman Aset</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="text-center">
-                <Card.Body>
-                  <h4>1</h4>
-                  <p>Permintaan Mutasi</p>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card className="text-center">
-                <Card.Body>
-                  <h4>0</h4>
-                  <p>Permintaan Laporan Aset</p>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Card>
-                <Card.Header className="bg-warning text-white">Jumlah Aset Berdasarkan Jenis</Card.Header>
-                <Card.Body>
-                  {/* <Pie data={pieData} /> */}
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <Card.Header className="bg-warning text-white">Kebutuhan Aset Berdasarkan Jenis</Card.Header>
-                <Card.Body>
-                  {/* <Bar data={barData} /> */}
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
+          <Container fluid className="mt-4">
+            <Row className="mb-4">
+              <Col>
+                <Card className="text-center">
+                  <Card.Body>
+                    <h4>1</h4>
+                    <p>Permintaan Aset</p>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="text-center">
+                  <Card.Body>
+                    <h4>1</h4>
+                    <p>Peminjaman Aset</p>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="text-center">
+                  <Card.Body>
+                    <h4>1</h4>
+                    <p>Permintaan Mutasi</p>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="text-center">
+                  <Card.Body>
+                    <h4>0</h4>
+                    <p>Permintaan Laporan Aset</p>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Header className="bg-warning text-white">Jumlah Aset Berdasarkan Jenis</Card.Header>
+                  <Card.Body>
+                    {/* <Pie data={pieData} /> */}
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <Card.Header className="bg-warning text-white">Kebutuhan Aset Berdasarkan Jenis</Card.Header>
+                  <Card.Body>
+                    {/* <Bar data={barData} /> */}
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
 
@@ -192,11 +159,11 @@ const Report = () => {
               <form onSubmit={addUser}>
                 <div className="form-group">
                   <label htmlFor="amount">Amounts</label>
-                  <div class="input-group mb-3">
-                    <span class="input-group-text rounded-0">Rp.</span>
+                  <div className="input-group mb-3">
+                    <span className="input-group-text rounded-0">Rp.</span>
                     <input
                       type="number"
-                      class="form-control"
+                      className="form-control"
                       aria-label="Amount (to the nearest dollar)"
                       id="amount"
                       name="amount"
@@ -206,7 +173,7 @@ const Report = () => {
                       placeholder="0"
                       required
                     />
-                    <span class="input-group-text rounded-0">.00</span>
+                    <span className="input-group-text rounded-0">.00</span>
                   </div>
                 </div>
                 <div className="form-group">
@@ -225,7 +192,7 @@ const Report = () => {
                   Submit
                 </button>
                 <button
-                  className="btn btn-secondary m-1 "
+                  className="btn btn-secondary m-1"
                   type="button"
                   data-dismiss="modal"
                 >
@@ -274,7 +241,7 @@ const Report = () => {
                   Delete
                 </button>
                 <button
-                  className="btn btn-secondary m-1 "
+                  className="btn btn-secondary m-1"
                   type="button"
                   data-dismiss="modal"
                 >
